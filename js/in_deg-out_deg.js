cba72f31f3ce954f0f34893f15d52a54985a89f0
Justin Jacques <56176145+YoungPenguin@users.noreply.github.com>
2021-11-20 15:42:36 +0100

    1

--- js/in_deg-out_deg.js
+++ js/in_deg-out_deg.js
@@ -1,91 +1,54 @@
-const in_degdoc = document.getElementById('in_deg');
-const in_deg = new Chart(in_degdoc, {
-    type: 'bar',
-    data: {
-        labels: [  0. ,  10.2,  20.4,  30.6,  40.8,  51. ,  61.2,  71.4,  81.6, 91.8, 102. ],
-        datasets: [{
-            label: 'In Degree Distribution of Gcc',
-            data: [138.,  63.,  25.,  15.,   5.,   7.,   4.,   3.,   0.,   1.],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
-                
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+var in_labels = [  0. ,  10.2,  20.4,  30.6,  40.8,  51. ,  61.2,  71.4,  81.6, 91.8, 102. ]
+var out_labels = [ 0. ,  8.4, 16.8, 25.2, 33.6, 42. , 50.4, 58.8, 67.2, 75.6, 84. ]
 
-const out_degdoc = document.getElementById('out_deg');
-const out_deg = new Chart(out_degdoc, {
+lineChart = new Chart(document.getElementById("InOutDeg"), {
     type: 'bar',
     data: {
-        labels: [ 0. ,  8.4, 16.8, 25.2, 33.6, 42. , 50.4, 58.8, 67.2, 75.6, 84. ],
+        labels: out_labels,
         datasets: [{
-            label: 'Out Degree Distribution of Gcc',
             data: [95., 85., 43., 12., 14.,  5.,  2.,  3.,  1.,  1.],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
-                
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
-            ],
             borderWidth: 1
         }]
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+        title: {
+            display: true,
+            text: 'Out Degree Distribution of Gcc'
+        },
+        legend: {
+        display: false
+    },
+    tooltips: {
+        callbacks: {
+           label: function(tooltipItem) {
+                  return tooltipItem.yLabel;
+           }
         }
+    }
     }
 });
 
 
+document.getElementById("Out-Degree").addEventListener("click", function() {
+  lineChart.config.data = {
+            labels: out_labels,
+            datasets: [{
+                data: [95., 85., 43., 12., 14.,  5.,  2.,  3.,  1.,  1.],
+
+            }],
+        }
+        lineChart.options.title.text = "Out Degree Distribution of Gcc"
+        lineChart.update();
+});
+
+document.getElementById("In-Degree").addEventListener("click", function() {
+  lineChart.config.data = {
+            labels: in_labels,
+            datasets: [{
+                data: [138.,  63.,  25.,  15.,   5.,   7.,   4.,   3.,   0.,   1.],
+
+            }],
+        }
+        lineChart.options.title.text = "In Degree Distribution of Gcc"
+        lineChart.update();
+});
