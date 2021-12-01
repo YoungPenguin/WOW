3607207a0da80cff96ac503807700244c03d4763
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-12-01 16:55:16 +0100

    color stuff

--- assets/js/dis.js
+++ assets/js/dis.js
@@ -2,186 +2,133 @@
 var gender_labels = ['Female', 'Male', 'Unknown']
 var fraction_labels = ['Alliance', 'Horde', 'Neutral']
 var status_lables = ['Alive', 'Deceased', 'Unknown']
-var race_labels = ['Air Elemental', 'Ancient Guardian', 'Annihilan', 'Arakkoa',
-'August Celestial', 'Bear', 'Black Drake', 'Blood Elf',
-'Blue Drake', 'Boar', 'Broken', 'Bronze Drake', 'Constellar',
-'Crypt Lord', 'Dark Iron Dwarf', 'Demon', 'Draenei', 'Dread Raven',
-'Drogbar', 'Dwarf', 'Earth Elemental', 'Fel Lord', 'Fel Orc',
-'Fire Elemental', 'Forest Troll', 'Forsaken', 'Frost Wyrm',
-'Gnome', 'Goblin', 'God', 'Green Drake', 'Gronn', 'Half-Elf',
-'Half-Ogre', 'Half-Orc', 'High Elf', 'Highmountain Tauren',
-'Hozen', 'Human', 'Ironforge Dwarf', 'Jungle Troll',
-'Keeper Of The Grove', 'Kyrian', 'Lich', 'Loa', "Mag'Har Orc",
-'Maldraxxi', "Man'Ari Eredar", 'Mantid', 'Mogu', "Mok'Nathal",
-'Mountain Dwarf', 'Naaru', 'Naga', 'Nathrezim', 'Night Elf',
-'Nightborne', 'Ogre', 'Ogre Mage', 'Old God', 'Orc', 'Pandaren',
-'Proto-Drake', 'Red Drake', 'Satyr', 'Stag', 'Tauren', 'Titan',
-'Titanic Watcher', 'Turtle', 'Undead', "Val'Kyr", 'Void Elf',
-'Vrykul', 'Water Elemental', 'Wild God', 'Wildhammer Dwarf',
-'Wisp', 'Worgen', 'Zandalari Troll']
+var race_labels = [
+    'Air Elemental', 'Ancient Guardian', 'Annihilan', 'Arakkoa',
+    'August Celestial', 'Bear', 'Black Drake', 'Blood Elf',
+    'Blue Drake', 'Boar', 'Broken', 'Bronze Drake', 'Constellar',
+    'Crypt Lord', 'Dark Iron Dwarf', 'Demon', 'Draenei', 'Dread Raven',
+    'Drogbar', 'Dwarf', 'Earth Elemental', 'Fel Lord', 'Fel Orc',
+    'Fire Elemental', 'Forest Troll', 'Forsaken', 'Frost Wyrm',
+    'Gnome', 'Goblin', 'God', 'Green Drake', 'Gronn', 'Half-Elf',
+    'Half-Ogre', 'Half-Orc', 'High Elf', 'Highmountain Tauren',
+    'Hozen', 'Human', 'Ironforge Dwarf', 'Jungle Troll',
+    'Keeper Of The Grove', 'Kyrian', 'Lich', 'Loa', "Mag'Har Orc",
+    'Maldraxxi', "Man'Ari Eredar", 'Mantid', 'Mogu', "Mok'Nathal",
+    'Mountain Dwarf', 'Naaru', 'Naga', 'Nathrezim', 'Night Elf',
+    'Nightborne', 'Ogre', 'Ogre Mage', 'Old God', 'Orc', 'Pandaren',
+    'Proto-Drake', 'Red Drake', 'Satyr', 'Stag', 'Tauren', 'Titan',
+    'Titanic Watcher', 'Turtle', 'Undead', "Val'Kyr", 'Void Elf',
+    'Vrykul', 'Water Elemental', 'Wild God', 'Wildhammer Dwarf',
+    'Wisp', 'Worgen', 'Zandalari Troll'
+]
 
 
 var chr = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    var data = {
-      type: "bar",
-      data: {
-        labels: race_labels,
-        datasets: [{
-          label: "Race",
-          data: [ 2,  1,  3,  2,  4,  2,  7,  9,  5,  1,  1,  5,  1,  1,  1,  1,  3,
-            1,  2,  2,  1,  1,  2,  2,  1,  7,  1,  3,  1,  1,  2,  1,  3,  1,
-            2,  4,  6,  1, 36,  2,  5,  3,  1,  1,  1,  3,  2,  2,  1,  1,  1,
-            2,  6,  1,  4,  9,  2,  1,  3,  4, 19,  9,  1,  3,  1,  1,  7,  7,
-            8,  1,  1,  3,  2,  1,  1,  2,  2,  1,  2,  4]
-        }]
-      }
-    };
-    var barchr = new Chart(ctx, data);
-    document.getElementById("gender").addEventListener("click", function() {
-        barchr.data = {
-                    labels: gender_labels,
-                    datasets: [{
-                        label: "Gender",
-                        data: [ 48, 199,  14],
-                    }],
-                }
-                barchr.update();
-        });
-        
-        document.getElementById("fraction").addEventListener("click", function() {
-          barchr.data = {
-                    labels: fraction_labels,
-                    datasets: [{
-                        label: "Faction",
-                        data: [ 41,  41, 179],
-                    }],
-                }
-                barchr.update();
-        });
-        
-        document.getElementById("status").addEventListener("click", function() {
-            barchr.config.data = {
-                      labels: status_lables,
-                      datasets: [{
-                        label: "Status",
-                          data: [150, 107,   4],
-          
-                      }],
-                  }
-                  barchr.update();
-          });
-          document.getElementById("race").addEventListener("click", function() {
-            barchr.config.data = {
-                      labels: race_labels,
-                      datasets: [{
-                        label: "Race",
-                          data: [ 2,  1,  3,  2,  4,  2,  7,  9,  5,  1,  1,  5,  1,  1,  1,  1,  3,
-                            1,  2,  2,  1,  1,  2,  2,  1,  7,  1,  3,  1,  1,  2,  1,  3,  1,
-                            2,  4,  6,  1, 36,  2,  5,  3,  1,  1,  1,  3,  2,  2,  1,  1,  1,
-                            2,  6,  1,  4,  9,  2,  1,  3,  4, 19,  9,  1,  3,  1,  1,  7,  7,
-                            8,  1,  1,  3,  2,  1,  1,  2,  2,  1,  2,  4],
-          
-                      }],
-                  }
-                  barchr.update();
-          });
-/*var gener_labels = ['Female', 'Male', 'Unknown']
-var fraction_labels = ['Alliance', 'Horde', 'Neutral']
-var status_lables = ['Alive', 'Deceased', 'Unknown']
-var race_labels = ['Air Elemental', 'Ancient Guardian', 'Annihilan', 'Arakkoa',
-'August Celestial', 'Bear', 'Black Drake', 'Blood Elf',
-'Blue Drake', 'Boar', 'Broken', 'Bronze Drake', 'Constellar',
-'Crypt Lord', 'Dark Iron Dwarf', 'Demon', 'Draenei', 'Dread Raven',
-'Drogbar', 'Dwarf', 'Earth Elemental', 'Fel Lord', 'Fel Orc',
-'Fire Elemental', 'Forest Troll', 'Forsaken', 'Frost Wyrm',
-'Gnome', 'Goblin', 'God', 'Green Drake', 'Gronn', 'Half-Elf',
-'Half-Ogre', 'Half-Orc', 'High Elf', 'Highmountain Tauren',
-'Hozen', 'Human', 'Ironforge Dwarf', 'Jungle Troll',
-'Keeper Of The Grove', 'Kyrian', 'Lich', 'Loa', "Mag'Har Orc",
-'Maldraxxi', "Man'Ari Eredar", 'Mantid', 'Mogu', "Mok'Nathal",
-'Mountain Dwarf', 'Naaru', 'Naga', 'Nathrezim', 'Night Elf',
-'Nightborne', 'Ogre', 'Ogre Mage', 'Old God', 'Orc', 'Pandaren',
-'Proto-Drake', 'Red Drake', 'Satyr', 'Stag', 'Tauren', 'Titan',
-'Titanic Watcher', 'Turtle', 'Undead', "Val'Kyr", 'Void Elf',
-'Vrykul', 'Water Elemental', 'Wild God', 'Wildhammer Dwarf',
-'Wisp', 'Worgen', 'Zandalari Troll']
-
-barChart = new Chart(document.getElementById("displot"), {
-    type: 'bar',
-    data: {
-        labels: gender_labels,
-        datasets: [{
-            data: [ 48, 199,  14],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Gender'
-        },
-        legend: {
+var ctx = canvas.getContext("2d");
+var options = {
+    scales: { yAxes: [{ ticks: { beginAtZero: true } }] },
+    legend: {
         display: false
     },
-    tooltips: {
-        callbacks: {
-           label: function(tooltipItem) {
-                  return tooltipItem.yLabel;
-           }
-        }
+}
+var data = {
+    type: "bar",
+    data: {
+        labels: race_labels,
+        datasets: [{
+            data: [
+                2, 1, 3, 2, 4, 2, 7, 9, 5, 1, 1, 5, 1, 1, 1, 1, 3,
+                1, 2, 2, 1, 1, 2, 2, 1, 7, 1, 3, 1, 1, 2, 1, 3, 1,
+                2, 4, 6, 1, 36, 2, 5, 3, 1, 1, 1, 3, 2, 2, 1, 1, 1,
+                2, 6, 1, 4, 9, 2, 1, 3, 4, 19, 9, 1, 3, 1, 1, 7, 7,
+                8, 1, 1, 3, 2, 1, 1, 2, 2, 1, 2, 4
+            ]
+        }]
+    },
+
+    options: options
+};
+
+var barchr = new Chart(ctx, data);
+document.getElementById("gender").addEventListener("click", function () {
+    barchr.data = {
+        labels: gender_labels,
+        datasets: [{
+            data: [48, 199, 14],
+            borderColor: [
+                labelToRGB['Female'][0],
+                labelToRGB['Male'][0],
+                labelToRGB['Unknown'][0],
+            ],
+            backgroundColor: [
+                labelToRGB['Female'][1],
+                labelToRGB['Male'][1],
+                labelToRGB['Unknown'][1],
+            ],
+            borderWidth: 1
+        }],
     }
+    barchr.config.options = options
+    barchr.update();
+});
+
+document.getElementById("fraction").addEventListener("click", function () {
+    barchr.data = {
+        labels: fraction_labels,
+        datasets: [{
+            data: [41, 41, 179],
+            borderColor: [
+                labelToRGB['Alliance'][0],
+                labelToRGB['Horde'][0],
+                labelToRGB['Neutral'][0],
+            ],
+            backgroundColor: [
+                labelToRGB['Alliance'][1],
+                labelToRGB['Horde'][1],
+                labelToRGB['Neutral'][1],
+            ],
+            borderWidth: 1
+        }],
     }
+    barchr.options = options
+    barchr.update();
 });
 
-
-document.getElementById("gender").addEventListener("click", function() {
-barChart.config.data = {
-            labels: gender_labels,
-            datasets: [{
-                data: [ 48, 199,  14],
-            }],
-        }
-        barChart.options.title.text = "genderr"
-        barChart.update();
+document.getElementById("status").addEventListener("click", function () {
+    barchr.config.data = {
+        labels: status_lables,
+        datasets: [{
+            data: [150, 107, 4],
+            borderColor: [
+                labelToRGB['Alive'][0],
+                labelToRGB['Deceased'][0],
+                labelToRGB['Unknown'][0],
+            ],
+            backgroundColor: [
+                labelToRGB['Alive'][1],
+                labelToRGB['Deceased'][1],
+                labelToRGB['Unknown'][1],
+            ],
+            borderWidth: 1
+        }],
+    }
+    barchr.config.options = options
+    barchr.update();
 });
 
-document.getElementById("fraction").addEventListener("click", function() {
-  lineChart.config.data = {
-            labels: fraction_labels,
-            datasets: [{
-                data: [ 41,  41, 179],
-
-            }],
-        }
-        lineChart.options.title.text = "fraction"
-        lineChart.update();
+document.getElementById("race").addEventListener("click", function () {
+    barchr.config.data = {
+        labels: race_labels,
+        datasets: [{
+            data: [
+                2, 1, 3, 2, 4, 2, 7, 9, 5, 1, 1, 5, 1, 1, 1, 1, 3,
+                1, 2, 2, 1, 1, 2, 2, 1, 7, 1, 3, 1, 1, 2, 1, 3, 1,
+                2, 4, 6, 1, 36, 2, 5, 3, 1, 1, 1, 3, 2, 2, 1, 1, 1,
+                2, 6, 1, 4, 9, 2, 1, 3, 4, 19, 9, 1, 3, 1, 1, 7, 7,
+                8, 1, 1, 3, 2, 1, 1, 2, 2, 1, 2, 4
+            ],
+        }],
+    }
+    barchr.config.options = options
+    barchr.update();
 });
-
-document.getElementById("status").addEventListener("click", function() {
-    lineChart.config.data = {
-              labels: status_labels,
-              datasets: [{
-                  data: [150, 107,   4],
-  
-              }],
-          }
-          lineChart.options.title.text = "status"
-          lineChart.update();
-  });
-  document.getElementById("race").addEventListener("click", function() {
-    lineChart.config.data = {
-              labels: race_labels,
-              datasets: [{
-                  data: [ 2,  1,  3,  2,  4,  2,  7,  9,  5,  1,  1,  5,  1,  1,  1,  1,  3,
-                    1,  2,  2,  1,  1,  2,  2,  1,  7,  1,  3,  1,  1,  2,  1,  3,  1,
-                    2,  4,  6,  1, 36,  2,  5,  3,  1,  1,  1,  3,  2,  2,  1,  1,  1,
-                    2,  6,  1,  4,  9,  2,  1,  3,  4, 19,  9,  1,  3,  1,  1,  7,  7,
-                    8,  1,  1,  3,  2,  1,  1,  2,  2,  1,  2,  4],
-  
-              }],
-          }
-          lineChart.options.title.text = "race"
-          lineChart.update();
-  });
-  */
--- assets/js/globals.js
+++ assets/js/globals.js
@@ -1,16 +1,35 @@
-const labelToColor = {
-    'Horde': '#FF3333',
-    'Alliance': '#3399FF',
-    'Neutral': '#A0A0A0',
-    'Male': '#3399FF',
-    'Female': '#D44ACC',
-    'Alive': '#27CF1F',
-    'Deceased': '#A0A0A0',
-    "Khadgar, Illidan, Lor'themar": '#808000',
-    "Deathwing, Sargeras, Yogg-Saron": '#ff4500',
-    "Sylvanas, Lich King, Varian Wrynn": '#c71585',
-    "Tyrande, Malfurion, Alexstrasza": '#00ff00	',
-    "Thrall, Ner'zhul, Orgrim": '#00ffff',
-    "Anzu, Terokk, Talon King Ikiss": '#0000ff',
-    "Jaina Proudmoore, Anduin Wrynn, Garrosh": '#1e90ff'
+// const labelToColor = {
+//     'Horde': '#FF3333',
+//     'Alliance': '#3399FF',
+//     'Neutral': '#A0A0A0',
+//     'Male': '#3399FF',
+//     'Female': '#D44ACC',
+//     'Alive': '#27CF1F',
+//     'Deceased': '#A0A0A0',
+//     "Khadgar, Illidan, Lor'themar": '#808000',
+//     "Deathwing, Sargeras, Yogg-Saron": '#ff4500',
+//     "Sylvanas, Lich King, Varian Wrynn": '#c71585',
+//     "Tyrande, Malfurion, Alexstrasza": '#00ff00	',
+//     "Thrall, Ner'zhul, Orgrim": '#00ffff',
+//     "Anzu, Terokk, Talon King Ikiss": '#0000ff',
+//     "Jaina Proudmoore, Anduin Wrynn, Garrosh": '#1e90ff'
+// }
+
+const labelToRGB = {
+    'Alive': ['rgb(34, 139, 34)', 'rgba(34, 139, 34, 0.2)'],
+    'Deceased': ['rgb(52, 52, 52)', 'rgba(52, 52, 52, 0.2)'],
+    'Unknown': ['rgb(160, 160, 160)', 'rgba(160, 160, 160, 0.2)'], 
+    'Alliance': ['rgb(51, 153, 255)', 'rgba(51, 153, 255, 0.2)'],
+    'Horde': ['rgb(255, 51, 51)', 'rgba(255, 51, 51, 0.2)'],
+    'Neutral': ['rgb(160, 160, 160)', 'rgba(160, 160, 160, 0.2)'],
+    'Male': ['rgb(51, 153, 255)', 'rgba(51, 153, 255, 0.2)'],
+    'Female': ['rgb(212, 74, 204)', 'rgba(212, 74, 204, 0.2)'],
+    "Khadgar, Illidan, Lor'themar": ['rgb(128, 128, 0)', 'rgba(128, 128, 0, 0.2)'],
+    "Deathwing, Sargeras, Yogg-Saron": ['rgb(255, 69, 0)', 'rgba(255, 69, 0, 0.2)'],
+    "Sylvanas, Lich King, Varian Wrynn": ['rgb(199, 21, 133)', 'rgba(199, 21, 133, 0.2)'],
+    "Tyrande, Malfurion, Alexstrasza": ['rgb(0, 255, 0)', 'rgba(0, 255, 0, 0.2)'],
+    "Thrall, Ner'zhul, Orgrim": ['rgb(0, 255, 255)', 'rgba(0, 255, 255, 0.2)'],
+    "Anzu, Terokk, Talon King Ikiss": ['rgb(0, 0, 255)', 'rgba(0, 0, 255, 0.2)'],
+    "Jaina Proudmoore, Anduin Wrynn, Garrosh": ['rgb(30, 144, 255)', 'rgba(30, 144, 255, 0.2)']
 }
+
