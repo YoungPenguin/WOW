388c367e7a45a47b9af352bafe825b4c23d2ac6d
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-12-01 17:15:57 +0100

    update color wave for communities

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -24,12 +24,12 @@ const labelToRGB = {
     'Neutral': ['rgb(160, 160, 160)', 'rgba(160, 160, 160, 0.2)'],
     'Male': ['rgb(51, 153, 255)', 'rgba(51, 153, 255, 0.2)'],
     'Female': ['rgb(212, 74, 204)', 'rgba(212, 74, 204, 0.2)'],
-    "Khadgar, Illidan, Lor'themar": ['rgb(128, 128, 0)', 'rgba(128, 128, 0, 0.2)'],
-    "Deathwing, Sargeras, Yogg-Saron": ['rgb(255, 69, 0)', 'rgba(255, 69, 0, 0.2)'],
-    "Sylvanas, Lich King, Varian Wrynn": ['rgb(199, 21, 133)', 'rgba(199, 21, 133, 0.2)'],
-    "Tyrande, Malfurion, Alexstrasza": ['rgb(0, 255, 0)', 'rgba(0, 255, 0, 0.2)'],
-    "Thrall, Ner'zhul, Orgrim": ['rgb(0, 255, 255)', 'rgba(0, 255, 255, 0.2)'],
-    "Anzu, Terokk, Talon King Ikiss": ['rgb(0, 0, 255)', 'rgba(0, 0, 255, 0.2)'],
-    "Jaina Proudmoore, Anduin Wrynn, Garrosh": ['rgb(30, 144, 255)', 'rgba(30, 144, 255, 0.2)']
+    "Khadgar, Illidan, Lor'themar": ['#62704d', 'rgba(128, 128, 0, 0.2)'],
+    "Deathwing, Sargeras, Yogg-Saron": ['#53868b', 'rgba(255, 69, 0, 0.2)'],
+    "Sylvanas, Lich King, Varian Wrynn": ['#8e635e', 'rgba(199, 21, 133, 0.2)'],
+    "Tyrande, Malfurion, Alexstrasza": ['#f5a791', 'rgba(0, 255, 0, 0.2)'],
+    "Thrall, Ner'zhul, Orgrim": ['#8c7549', 'rgba(0, 255, 255, 0.2)'],
+    "Anzu, Terokk, Talon King Ikiss": ['#b3c598', 'rgba(0, 0, 255, 0.2)'],
+    "Jaina Proudmoore, Anduin Wrynn, Garrosh": ['#432f20', 'rgba(30, 144, 255, 0.2)']
 }
 
