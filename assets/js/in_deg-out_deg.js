837ea4e99776e3779277985797947be76ab27e0e
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-12-01 15:28:40 +0100

    remove console log

--- assets/js/in_deg-out_deg.js
+++ assets/js/in_deg-out_deg.js
@@ -889,7 +889,6 @@ var data1 = [{
 
 
 document.getElementById("corr").addEventListener("click", function() {
-console.log("dssdds")
     lineChart.destroy();
     lineChart = new Chart(document.getElementById("InOutDeg"), {
         type: 'scatter',
