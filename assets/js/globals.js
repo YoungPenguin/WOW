a5e8652014dffd5dcff53d222dda958b880474fa
Lucas Sorensen <lucas.alex.sorensen@gmail.com>
2021-12-01 16:27:54 +0100

    Add file for global vars. also update colors for TS plot

---
+++ assets/js/globals.js
@@ -0,0 +1,16 @@
+const labelToColor = {
+    'Horde': '#FF3333',
+    'Alliance': '#3399FF',
+    'Neutral': '#A0A0A0',
+    'Male': '#3399FF',
+    'Female': '#D44ACC',
+    'Alive': '#27CF1F',
+    'Deceased': '#A0A0A0',
+    "Khadgar, Illidan, Lor'themar": '#808000',
+    "Deathwing, Sargeras, Yogg-Saron": '#ff4500',
+    "Sylvanas, Lich King, Varian Wrynn": '#c71585',
+    "Tyrande, Malfurion, Alexstrasza": '#00ff00	',
+    "Thrall, Ner'zhul, Orgrim": '#00ffff',
+    "Anzu, Terokk, Talon King Ikiss": '#0000ff',
+    "Jaina Proudmoore, Anduin Wrynn, Garrosh": '#1e90ff'
+}
--- assets/js/tsComments.js
+++ assets/js/tsComments.js
@@ -1,20 +1,3 @@
-const labelToColor = {
-    'Horde': '#FF3333',
-    'Alliance': '#3399FF',
-    'Neutral': '#A0A0A0',
-    'Male': '#3399FF',
-    'Female': '#D44ACC',
-    'Alive': '#27CF1F',
-    'Deceased': '#A0A0A0',
-    "Khadgar, Illidan, Lor'themar": '#808000',
-    "Deathwing, Sargeras, Yogg-Saron": '#ff4500',
-    "Sylvanas, Lich King, Varian Wrynn": '#c71585',
-    "Tyrande, Malfurion, Alexstrasza": '#00ff00	',
-    "Thrall, Ner'zhul, Orgrim": '#00ffff',
-    "Anzu, Terokk, Talon King Ikiss": '#0000ff',
-    "Jaina Proudmoore, Anduin Wrynn, Garrosh": '#1e90ff'
-}
-
 function plotTs() {
     params = {
         metric: document.getElementById("ts_metric").value,
@@ -27,12 +10,13 @@ function plotTs() {
     )
     const plotData = lines.map(l => {
         const line = { width: 4 }
-        if (labelToColor[l.label]) {
-            line.color = labelToColor[l.label]
+        const label = l.label.replace(' Stormrage', '').replace(' Windrunner', '').replace(' Theron', '').replace(' Whisperwind', '').replace(' Doomhammer', '').replace(' Hellscream', '')
+        if (labelToColor[label]) {
+            line.color = labelToColor[label]
         }
         return {
             mode: 'lines+markers',
-            name: l.label.replace(' Stormrage', '').replace(' Windrunner', '').replace(' Theron', '').replace(' Whisperwind', '').replace(' Doomhammer', '').replace(' Hellscream', ''),
+            name: label,
             y: l.y,
             x: l.x,
             line,
